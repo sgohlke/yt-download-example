bfffe4a2a31f5734d1910cdb1903facbf976621f
sgohlke <gohlke.stefan@yahoo.de>
2024-03-10 15:12:12 +0100

    Update to converter v2

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const yt = require("yt-converter");
-const videoId = 'IM89MNpdbXc'
-const videoUrl = `https://www.youtube.com/watch?v=${videoId}`
-yt.getInfo(videoUrl).then(info => {
-    // console.log(info);
-    const title = info.title
-    const audioLane = info.formatsAudio.find( audioinfo => audioinfo.audioBitrate === 128 )
-    if (audioLane) {
-        const audioItag = audioLane.itag
-        yt.convertAudio({
-            url: videoUrl,
-            itag: audioItag,
-            directoryDownload: __dirname,
-            title: title
-        }, undefined, undefined)
-    }
-});
 
+const videoId = 'IM89MNpdbXc'
+const link = `https://www.youtube.com/watch?v=${videoId}`
+
+async function download(url) {
+  const data = await yt.Audio({
+    url,
+    onDownloading: (d) => console.log(d),
+  });
+
+  console.log(data.message);
+}
+
+download(link);
